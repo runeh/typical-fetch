e9b568c2ed4a13b546ee1b1124ddf3f40bde6e87
Rune <runefh@gmail.com>
2021-05-05 19:42:22 +0200

    feat: Initial version

---
+++ examples/runtypes.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+
+import * as rt from 'runtypes';
+import { buildCall, unwrapError } from '../src/index';
+
+const baseUrl = 'https://httpbin.org';
+
+const jsonData = { people: [{ name: 'Rune' }, { name: 'David' }] };
+
+const isPeopleResponse = rt.Record({
+  json: rt.Record({
+    people: rt.Array(rt.Record({ name: rt.String })),
+  }),
+});
+
+function withRuntype<T>(validator: rt.Runtype<T>) {
+  return (data: unknown) => {
+    return validator.check(data);
+  };
+}
+
+async function parseWithRuntypes() {
+  const getRequest = buildCall()
+    .path('/anything')
+    .method('post')
+    .body(jsonData)
+    .parseJson(withRuntype(isPeopleResponse))
+    .mapError((err) => {
+      const unwrapped = unwrapError(err);
+      return unwrapped instanceof rt.ValidationError ? unwrapped : err;
+    })
+
+    .build();
+
+  const res = await getRequest({ baseUrl });
+
+  if (res.success) {
+    console.log(res.body.json);
+  } else {
+    console.log(res.error.name);
+  }
+}
+
+async function main() {
+  await parseWithRuntypes();
+}
+
+main();
---
+++ src/types.ts
@@ -0,0 +1,87 @@
+import { Readable } from 'stream';
+import { URL, URLSearchParams } from 'url';
+import {
+  HeadersInit,
+  BodyInit as OriginalBodyInit,
+  RequestInit,
+  Response,
+} from 'node-fetch';
+
+export type CallReturn<Ret, Err> =
+  | { success: true; body: Ret; error: undefined }
+  | { success: false; body: undefined; error: Err };
+
+// The `[]` is due to this:
+// https://github.com/microsoft/TypeScript/issues/23182#issuecomment-379091887
+
+/**
+ * The shape of the function returned by `.build()`. This conditional type
+ * makes sure that the function takes no arguments if `Arg` is `never` or an
+ * empty object, `Record<string, never>`.
+ * Otherwise, the function takes Arg as a single argument
+ */
+export type FetchCall<Ret, Arg, Err> = [Arg] extends [never]
+  ? () => Promise<CallReturn<Ret, Err>>
+  : Arg extends Record<string, never>
+  ? () => Promise<CallReturn<Ret, Err>>
+  : (args: Arg) => Promise<CallReturn<Ret, Err>>;
+
+export type MergedArgs<OldArg, NewArg> = [OldArg] extends [never]
+  ? Readonly<NewArg>
+  : Readonly<OldArg & NewArg>;
+
+export type BodyInit =
+  | Exclude<OriginalBodyInit, ArrayBufferView | NodeJS.ReadableStream>
+  | Readable;
+
+export type HttpMethod = 'delete' | 'get' | 'head' | 'patch' | 'post' | 'put';
+
+export type QueryParam = Record<string, string> | URLSearchParams;
+
+export type BodyType = JsonRoot | BodyInit;
+
+export interface CallRecord {
+  readonly baseUrl?: URL;
+  readonly errorMappers: ((error: any, arg: any) => unknown)[];
+  readonly getBody?: (arg: any) => BodyType;
+  readonly getHeaders: ((arg: any) => HeadersInit)[];
+  readonly getPath?: (arg: any) => string;
+  readonly getQuery: ((arg: any) => QueryParam)[];
+  readonly mapError: ((arg: any) => unknown)[];
+  readonly mappers: ((res: any, arg: any) => unknown)[];
+  readonly method?: HttpMethod;
+  readonly parseJson?: (json: unknown, arg: any) => unknown;
+  readonly parseResponse?: (res: Response, arg: any) => unknown;
+  readonly parseText?: (body: string, arg: any) => unknown;
+  readonly requestInit?: TypicalRequestInit;
+}
+
+export class TypicalWrappedError extends Error {
+  name = 'TypicalWrappedError';
+  constructor(
+    public wrappedError: unknown,
+    public response: Response | undefined,
+    public bodyText: string | undefined,
+  ) {
+    super(wrappedError instanceof Error ? wrappedError.name : 'unknown');
+  }
+}
+
+export class TypicalHttpError extends Error {
+  name = 'TypicalHttpError';
+  constructor(public status: number, message?: string) {
+    super(message ?? `Status: ${status}`);
+  }
+}
+
+export type TypicalRequestInit = Pick<RequestInit, 'redirect'>;
+
+interface JsonObject {
+  [member: string]: JsonValue;
+}
+
+type JsonArray = Array<JsonValue>;
+
+type JsonValue = string | number | boolean | null | JsonObject | JsonArray;
+
+export type JsonRoot = JsonArray | JsonObject;
